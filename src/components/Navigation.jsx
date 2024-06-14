e6089778f9ec62a8838cf840df6524358115afda
Krrish <www.monikchatterjee@gmail.com>
2024-06-14 17:52:45 +0530

    removed shephard from about page

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,5 +1,3 @@
-import { useEffect } from "react";
-import Shepherd from "shepherd.js";
 import { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import search from "../assets/nav-search.png";
@@ -12,101 +10,6 @@ import { ThemeContext } from "./ThemeContext";
 
 const Navigation = () => {
   const { isDarkMode, toggleDarkMode } = useContext(ThemeContext);
-
-  useEffect(() => {
-    const tour = new Shepherd.Tour({
-      defaultStepOptions: {
-        cancelIcon: {
-          enabled: true,
-        },
-        classes: "shepherd-theme-arrows",
-        scrollTo: { behavior: "smooth", block: "center" },
-      },
-    });
-
-    tour.addStep({
-      id: "home-step",
-      text: "This is the Home page where you can get an overview of my portfolio.",
-      attachTo: { element: ".home", on: "bottom" },
-      buttons: [
-        {
-          text: "Next",
-          action: tour.next,
-        },
-      ],
-    });
-
-    tour.addStep({
-      id: "about-step",
-      text: "This is the About page where you can learn more about me.",
-      attachTo: { element: ".about", on: "bottom" },
-      buttons: [
-        {
-          text: "Next",
-          action: tour.next,
-        },
-        {
-          text: "Back",
-          action: tour.back,
-        },
-      ],
-    });
-
-    // Add more steps for other navigation items...
-    tour.addStep({
-      id: "projects-step",
-      text: "This is the Projects page where you can see my projects.",
-      attachTo: { element: ".projects", on: "bottom" },
-      buttons: [
-        {
-          text: "Next",
-          action: tour.next,
-        },
-        {
-          text: "Back",
-          action: tour.back,
-        },
-      ],
-    });
-
-    tour.addStep({
-      id: "contact-step",
-      text: "This is the Contact page where you can contact me.",
-      attachTo: { element: ".contact", on: "bottom" },
-      buttons: [
-        {
-          text: "Next",
-          action: tour.next,
-        },
-        {
-          text: "Back",
-          action: tour.back,
-        },
-      ],
-    });
-
-    tour.addStep({
-      id: "github-step",
-      text: "This is the GitHub link where you can see my repositories.",
-      attachTo: { element: ".git-hub", on: "bottom" },
-      buttons: [
-        {
-          text: "Done",
-          action: tour.next,
-        },
-        {
-          text: "Back",
-          action: tour.back,
-        },
-      ],
-    });
-
-    tour.start();
-
-    return () => {
-      tour.complete();
-    };
-  }, []);
 
   return (
     <section
